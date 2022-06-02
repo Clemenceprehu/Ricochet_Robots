2c882f9eea7f014fff57ddaf5d460074305c5ef2
lucief24 <92854756+lucief24@users.noreply.github.com>
2022-06-02 20:48:13 +0200

    board

--- src/main/java/com/example/ricochet_robots/board/Bot.java
+++ src/main/java/com/example/ricochet_robots/board/Bot.java
@@ -4,4 +4,29 @@ import java.awt.*;
 
 public class Bot {
 
+    private BotColor botcolor;
+    private Point position;
+
+    public Bot(BotColor botcolor, Point position) {
+        this.botcolor = botcolor;
+        this.position = position;
+    }
+
+    public BotColor getBotcolor() {
+        return botcolor;
+    }
+
+    public void setBotcolor(BotColor botcolor) {
+        this.botcolor = botcolor;
+    }
+
+    public Point getPosition() {
+        return position;
+    }
+
+    public void setPosition(Point position) {
+        this.position = position;
+    }
+
+
 }
--- src/main/java/com/example/ricochet_robots/board/BotColor.java
+++ src/main/java/com/example/ricochet_robots/board/BotColor.java
@@ -2,4 +2,8 @@ package com.example.ricochet_robots.board;
 
 public enum BotColor {
 
+    Red("R"), Blue("B"), Green("G"), Yellow("Y"), None("N");
+    public final String label;
+    BotColor(String label) {
+        this.label = label;
 }
