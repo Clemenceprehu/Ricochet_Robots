403957e6b2628a9261f7bcec82196eaef0b0c397
Clemenceprehu <clemenceprehu@gmail.com>
2022-06-02 22:17:43 +0200

    objec

--- src/main/java/com/example/ricochet_robots/board/ObjectiveColor.java
+++ src/main/java/com/example/ricochet_robots/board/ObjectiveColor.java
@@ -1,6 +1,10 @@
 package com.example.ricochet_robots.board;
 
 public enum ObjectiveColor {
-    None
+    Red("R"), Blue("B"), Green("G"), Yellow("Y"), None("N");
 
+    public final String label;
+    ObjectiveColor(String label) {
+        this.label = label;
+    }
 }
